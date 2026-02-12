7ce54871c83986f5f7f59962fa651e19ec57d90a
Patel Helly <hkpatel1677@gmail.com>
2026-02-12 14:21:07 +0530

    fe2
    fe2

---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./Register.css";
+import { useNavigate } from "react-router-dom";
+
+function Register() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [role, setRole] = useState("student");
+
+  const navigate = useNavigate();
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await axios.post("http://localhost:5000/api/auth/register", {
+        name,
+        email,
+        password,
+        role,
+      });
+
+      alert("Registration Successful ✅");
+      navigate("/"); // redirect to login
+
+    } catch (error) {
+      alert(error.response?.data?.message || "Registration Failed ❌");
+    }
+  };
+
+  return (
+    <div className="register-container">
+      <form className="register-form" onSubmit={handleRegister}>
+        <h2>Register</h2>
+
+        <input
+          type="text"
+          placeholder="Enter Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+
+        <input
+          type="email"
+          placeholder="Enter Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+
+        <input
+          type="password"
+          placeholder="Enter Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+
+        <select value={role} onChange={(e) => setRole(e.target.value)}>
+          <option value="student">Student</option>
+          <option value="alumni">Alumni</option>
+        </select>
+
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+}
+
+export default Register;
