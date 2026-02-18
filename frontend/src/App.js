004db19e0334458656071b11ebbfcd07a64b3584
HusainaMahudawala <husaina.mahudawala@gmail.com>
2026-02-18 14:30:47 +0530

    My local changes before pulling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import StudentDashboard from "./pages/StudentDashboard";
-import AlumniDashboard from "./pages/AlumniDashboard";
 
 function App() {
   return (
@@ -10,8 +8,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/student" element={<StudentDashboard />} />
-        <Route path="/alumni" element={<AlumniDashboard />} />
       </Routes>
     </Router>
   );
